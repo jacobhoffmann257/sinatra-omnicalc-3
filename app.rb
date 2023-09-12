24eb6b2c7214ef7ad121720ca1606fad860d69a8
Jacob Hoffmann <67609232+jacobhoffmann257@users.noreply.github.com>
2023-09-12 00:19:53 +0000

    changed to post

--- app.rb
+++ app.rb
@@ -1,6 +1,6 @@
 require "sinatra"
 require "sinatra/reloader"
-
+require "http"
 get("/") do
   "
   <h1>Welcome to your Sinatra App!</h1>
@@ -10,7 +10,15 @@ end
 get("/umbrella") do
 erb(:umbrella_form)
 end
-get("/proccess_umbrella") do
-  @user_location = params.fetch("user_loc")
+post("/proccess_umbrella") do
+  #this code wont work was sumurized
+#@user_location = params.fetch("user_loc")
+#gmaps_url = ""
+#@raw_response = HTTP.get(gmaps_url).to_s
+#@parsed_response = JSON.parse(@raw_response)
+#loc_hash = @parsed_response.dig("results",0,"geometry","location")
+#@lat = loc_hash.fetch("lat")
+#@long = loc_hash.fetch("long")
 erb(:umbrella_results)
+
 end
