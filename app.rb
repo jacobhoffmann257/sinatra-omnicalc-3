186db44135cc03cdd10638fbf0472dace53ac88e
Jacob Hoffmann <67609232+jacobhoffmann257@users.noreply.github.com>
2023-09-12 23:25:38 +0000

    umbrella done

--- app.rb
+++ app.rb
@@ -14,19 +14,46 @@ get("/umbrella") do
 erb(:umbrella_form)
 end
 post("/proccess_umbrella") do
-  #this code wont work was sumurized
-#@user_location = params.fetch("user_loc")
-#gmaps_url = ""
-#@raw_response = HTTP.get(gmaps_url).to_s
-#@parsed_response = JSON.parse(@raw_response)
-#loc_hash = @parsed_response.dig("results",0,"geometry","location")
-#@lat = loc_hash.fetch("lat")
-#@long = loc_hash.fetch("long")
+gmaps_key = ENV.fetch("GMAPS_KEY")
+@user_location = params.fetch("user_loc")
+gmaps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{@user_location}&key=#{gmaps_key}"
+raw_response = HTTP.get(gmaps_url).to_s
+parsed_response = JSON.parse(raw_response)
+loc_hash = parsed_response.dig("results",0,"geometry","location")
+lat = loc_hash.fetch("lat")
+long = loc_hash.fetch("lng")
+@long_string = lat.to_s
+@lat_string = long.to_s
 cookies["last_location"] = $user_location
-cookies["last_lat"] = "i"
-cookies["last_long"] = "h"
+cookies["last_lat"] = @lat_string
+cookies["last_long"] = @long_string
+#parsing pirate api
+pirate_key = ENV.fetch("PIRATE_WEATHER_KEY")
+pirate_url = "https://api.pirateweather.net/forecast/#{pirate_key}/#{long},#{lat}"
+raw_pirate_data = HTTP.get(pirate_url)
+parsed_pirate_data = JSON.parse(raw_pirate_data)
+current_pirate = parsed_pirate_data.fetch("currently")
+currently_temp = current_pirate.fetch("temperature")
+@long_lat = "The Longitude and latitude is #{@long_string}, #{@lat_string}"
+@temperature_string = "The Current tempature in #{@user_location} is #{currently_temp}"
+hour_parse = parsed_pirate_data.fetch("hourly")
+hour_data = hour_parse.fetch("data")
+next_twelve = hour_data[1..12]
+will_rain = 0
+count = 0
+next_twelve.each do |x|
+  if(x.fetch("precipProbability")>=10)
+    will_rain = will_rain + 1
+  end
+  
+end
+@answer = ""
+if(will_rain >= 1)
+  @answer = "You need an Umbrella"
+  else
+  @answer = "You don't need an Umbrella"
+  end
 erb(:umbrella_results)
-
 end
 post("/openai") do
 end
